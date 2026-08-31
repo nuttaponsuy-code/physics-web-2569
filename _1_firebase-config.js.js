e452f483026644844811f30729ba263ef55e65cb
nuttaponsuy-code <nuttapon.suy@thaimooc.ac.th>
2026-08-31 11:04:16 +0700

    Add files via upload

---
+++ _1_firebase-config.js.js
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+apiKey: "AIzaSyBg6PQHuldDCDFritxryxvw3mOl9E-aBBk",
+  authDomain: "physics-classroom-cab2b.firebaseapp.com",
+  projectId: "physics-classroom-cab2b",
+  storageBucket: "physics-classroom-cab2b.firebasestorage.app",
+  messagingSenderId: "942217956980",
+  appId: "1:942217956980:web:81356e9e6f7cd0738d8b21"
+};
+
+export const APPS_SCRIPT_URL = "https://script.googleusercontent.com/a/macros/thaimooc.ac.th/echo?user_content_key=AUkAhnTE_10HU1BnL3pDu8RVxxJHcvi-VJ7D_s36PaDqpiU9maQeD9xEtcLdpkJChGNpaAHPwQElKr41VUjzjeqHyFOLwsK0AJVmvVF7biUmh3yv4xtBHPY-E536Qpj9R8k-FHiflOrQsUDG2YEfjXvpVhwZREwSvgfLvQf3OAq9lsI6pAyEh-gPPAnZ1o-v8yPpbbuQVFaOfn4aeLKlHx0b9RjVhw0umKaKi5YCrtQ7N_QvuFHOESLbpTBQYxCbYPVUIwnl35HRJ5s74Ana_KE8gyYKwDbytg9ykHwQBvN_BaxmGEZs1Eo&lib=Mozu02Vw3c74gFuJ1j3UGoai60Hw58VBZ";
+
+// ===== รายชื่อห้องเรียนทั้งหมด =====
+
+export const CLASS_LIST = ["5-1", "5-2", "5-3", "6-1", "6-2", "6-3"];
